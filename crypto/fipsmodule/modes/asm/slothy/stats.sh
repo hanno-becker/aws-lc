586eb84129fd00750c19d7f65132b72f84d831ee
Hanno Becker <beckphan@amazon.co.uk>
2024-06-17 11:51:40 +0100

    Improve documentation of stats.sh script

--- crypto/fipsmodule/modes/asm/slothy/stats.sh
+++ crypto/fipsmodule/modes/asm/slothy/stats.sh
@@ -1,12 +1,39 @@
 #!/usr/bin/env sh
 set -e
 
+# This script processes a benchmark file like benchmarks_g2.md
+# Every line in the output describes the performance of one AES-GCM
+# variant. The legend of every individual line is:
+#
+# - {NAME}
+# - {MB/s (16b)}
+# - {MB/s (256b)}
+# - {MB/s (1350b)}
+# - {MB/s (8192b)}
+# - {MB/s (16384b)
+# - Expected cycles (SLOTHY-optimized code only)
+# - Expected IPC    (SLOTHY-optimized code only)
+# - Cycle bound     (SLOTHY-optimized code only)
+# - IPC bound       (SLOTHY-optimized code only)
+# - Wall time       (SLOTHY-optimized code only)
+# - User time       (SLOTHY-optimized code only)
+
 BENCHMARKS=${BENCHMARKS:=benchmarks_g2.md}
 if [ ! -f ${BENCHMARKS} ]; then
    echo "Benchmark file not found: ${BENCHMARKS}"
    exit 1
-else
-    echo "OK"
+fi
+
+SZ_ARG=$1
+if [ "$SZ" = "" ]; then
+    SZ_ARG=128
+    echo "No keysize specified via SZ_ARG -- defaulting to ${SZ_ARG} bit"
+fi
+
+UNROLL_ARG=$2
+if [ "$UNROLL_ARG" = "" ]; then
+    UNROLL_ARG="x4"
+    echo "No unrolling level specified UNROLL_ARG -- defaulting to ${UNROLL_ARG}"
 fi
 
 LOOP_LABEL="Lloop_unrolled_start:"
@@ -20,6 +47,8 @@ list_variants() {
 }
 
 get_benchmark_for() {
+    # Extracts and prints, separated by spaces, the throughputs (in MB/s)
+    # for 16, 256, 1350, 8k, 16k data sizes.
     SZ=$1
     UNROLL=$2
     DIR=$3
@@ -33,6 +62,13 @@ get_benchmark_for() {
 }
 
 get_slothy_stats_for() {
+    # Extracts and prints, separated by spaces, the following SLOTHY statistics:
+    # - Expected cycles
+    # - Expected IPC
+    # - Cycle bound
+    # - IPC bound
+    # - Wall time
+    # - User time
     SZ=$1
     UNROLL=$2
     DIR=$3
@@ -72,12 +108,16 @@ get_all_stats() {
     done
 }
 
-TYPES=$3
-if [ "$TYPES" = "" ]; then
-    TYPES="clean opt"
+TYPES_ARG=$3
+if [ "$TYPES_ARG" = "" ]; then
+    TYPES_ARG="clean opt"
 fi
 
-for T in $TYPES;
+# Get reference performance
+REF=$(get_benchmark_for $SZ_ARG $UNROLL_ARG aws-lc main)
+REF_ARR=(${REF})
+echo "main $REF - - - - - - -"
+for T in $TYPES_ARG;
 do
-    get_all_stats $1 $2 $T
+    get_all_stats $SZ_ARG $UNROLL_ARG $T
 done
